a04a2ae101f4529f114f5a62729be99dc9270553
Jhonatan Ferri <jhowvore@gmail.com>
2020-10-14 22:21:33 -0300

    subindo atividade 8

---
+++ Atividade8/js/script.js
@@ -0,0 +1,14 @@
+var aluno1 = new Object();
+aluno1.ra = "0030481823022";
+aluno1.nome = "Jhonatan";
+
+
+var aluno2 = {};
+aluno2.ra = "0030481823022";
+aluno2.nome = "Jhonatan";
+
+var aluno3 = {
+	ra: "0030481823022",
+	nome: "Jhonatan"
+};
+	
